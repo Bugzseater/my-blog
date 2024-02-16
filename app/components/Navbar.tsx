f1bddb5e541489bc28a87a372d641444da45077b
Bugzeater <madushanpasindu0123@gmail.com>
2024-02-16 20:53:26 +0530

    namechange

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar(){
     return(
         <nav className=" w-full relative flex items-center justify-between max-w-2xl mx-auto px-1 py-5">
             <Link href="/" className=" font-bold text-3xl">
-                Pasindu<span className=" text-primary">Blog</span>
+                Madushan<span className=" text-primary">Blog</span>
             </Link>
             <ModeToggle/>
         </nav>
